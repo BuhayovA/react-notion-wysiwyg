fd734f8f0eab55160cf212d557c1e2c9e573e1b2
Alexey Buhayov <neznaykagm@gmail.com>
2024-02-04 15:26:02 +0200

    hot-fix

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { Button } from "./Button";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
-const meta = {
+const meta: Meta<typeof Button> = {
   title: "Example/Button",
   component: Button,
   parameters: {
