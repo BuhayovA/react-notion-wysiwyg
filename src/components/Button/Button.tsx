447d2b9151721520ea7df1568d48a40d6d64342a
Alexey Buhayov <neznaykagm@gmail.com>
2024-02-04 13:36:32 +0200

    added terser optimization for rollup & added tailwindcss

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "./index.css";
 
 export interface ButtonProps {
   label: string;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <button className="bg-blue-400">{props.label}</button>;
 };
 
 export default Button;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  safelist: ["ProseMirror"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+};
